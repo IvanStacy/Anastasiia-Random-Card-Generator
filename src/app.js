404a81df73e720b7614d25a10243cca48c4a9e46
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2024-01-03 16:37:29 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomSuit = generateRandomSuit();
+  document.querySelector(".card").classList.add(randomSuit);
+  document.querySelector(".top-suit").innerHTML = randomSuit;
+  document.querySelector(".botton-suit").innerHTML = randomSuit;
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+
+let generateRandomSuit = () => {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let indexsuit = Math.floor(Math.random() * suit.length);
+  return suit[indexsuit];
+};
+
+let generateRandomNumber = () => {
+  let number = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let indexnumber = Math.floor(Math.random() * number.length);
+  return number[indexnumber];
 };
